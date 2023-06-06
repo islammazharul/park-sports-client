f4e0654281efe6aff75c147c29d053a5a56f68c7
islammazharul <islammazharul3645@gmail.com>
2023-06-07 03:10:02 +0600

    added footer

---
+++ park-sport-clientt/src/pages/Home/PopularInstructor/PopularInstructor.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import SectionTitle from '../../../components/SectionTitle/SectionTitle';
+
+const PopularInstructor = () => {
+    return (
+        <div>
+            <SectionTitle heading="Most Popular Instructors" subHeading="We thrive on building a player’s technical foundation and applying those skills into an appropriate competitive environment.  Most of our players have made a long-term commitment to become the best player they can, knowing that it involves hard work and dedication to reach their full potential."></SectionTitle>
+
+        </div>
+    );
+};
+
+export default PopularInstructor;
