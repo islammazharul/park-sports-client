c66f3f82fff5672d02cb5814d061e7685de4e6fb
islammazharul <islammazharul3645@gmail.com>
2023-06-09 23:25:08 +0600

    added class details in manage class

---
+++ park-sport-clientt/src/pages/Dashboard/Admin/ManageClasses/ManageCard/ManageCard.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+const ManageCard = ({ sport }) => {
+    const { image, class_name, email, price, status, total_enroll, available_seat, feedback, instructor_name } = sport;
+    return (
+        <div className=" mx-auto rounded-lg shadow-md lg:flex md:flex shadow-sky-600">
+            <img
+                className="object-cover w-full md:w-1/2 lg:w-1/2"
+
+                src={image}
+                alt="image"
+            />
+            <div className="px-6 py-4">
+                <h4 className="mb-3  text-base font-bold tracking-tight text-sky-600">
+                    Class Name :  {class_name}
+                </h4>
+                <h4 className="mb-3  text-base font-bold tracking-tight text-sky-600">
+                    Instructor Name :  {instructor_name}
+                </h4>
+                <h4 className="mb-3  text-base font-bold tracking-tight text-sky-600">
+                    Email :  {email}
+                </h4>
+                <h4 className="mb-3 text-base font-semibold tracking-tight text-sky-600">
+                    Status :  {status}
+                </h4>
+                <h4 className="mb-3 text-base font-semibold tracking-tight text-sky-600">
+                    Available Seats :  {available_seat}
+                </h4>
+                <h4 className="mb-3 text-base font-semibold tracking-tight text-sky-600">
+                    Price : $ {price}
+                </h4>
+                <div className='flex justify-between items-center'>
+                    <button className="btn btn-sm bg-green-500">Approve</button>
+                    <button className="btn btn-sm bg-red-500">Deny</button>
+                    <button className="btn btn-sm bg-yellow-500">Feedback</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ManageCard;
--- park-sport-clientt/src/pages/Dashboard/Admin/ManageClasses/ManageClasses.jsx
+++ park-sport-clientt/src/pages/Dashboard/Admin/ManageClasses/ManageClasses.jsx
@@ -1,9 +1,36 @@
 import React from 'react';
+import useAxiosSecure from '../../../../hooks/useAxiosSecure';
+import { useQuery } from '@tanstack/react-query';
+import { Helmet } from 'react-helmet-async';
+import ManageCard from './ManageCard/ManageCard';
+import SectionTitle from '../../../../components/SectionTitle/SectionTitle';
+
 
 const ManageClasses = () => {
+    const [axiosSecure] = useAxiosSecure();
+    const { data: sports = [], refetch, loading } = useQuery({
+        queryKey: ["sports"],
+        queryFn: async () => {
+            const res = await axiosSecure.get("/sports")
+            return res.data
+        }
+    })
     return (
-        <div>
-            <h2>Manage Classes</h2>
+        <div className='w-full max-h-full'>
+            <Helmet>
+                <title>Manage Classes | Park Sports</title>
+            </Helmet>
+            <SectionTitle heading="MANAGE ALL CLASSES"></SectionTitle>
+            <div className='grid grid-cols-1 md:grid-cols-2 gap-4 mt-10'>
+                {
+                    sports.map(sport => <ManageCard
+                        key={sport._id}
+                        sport={sport}
+                    ></ManageCard>
+                    )
+                }
+            </div>
+
         </div>
     );
 };
